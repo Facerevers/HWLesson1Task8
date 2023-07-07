06ddaf3b6a8dffe9ff263d5e0283a7b4a22c3459
crazle <crazle@mail.ru>
2023-07-07 18:09:47 +0300

    Создана программа, выводящая все чётные числа от 1 до N

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число и нажмите Enter");
+double a = double.Parse(Console.ReadLine());
+for (double count = 1; count < a+1; count++)
+{
+    if (count % 2 == 0) Console.Write($" {count}");
+}
